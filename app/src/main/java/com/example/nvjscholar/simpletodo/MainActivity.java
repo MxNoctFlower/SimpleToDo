7245a185ec5adc571d7246b765705f16795f66c6
Nigel Van Jacobs <nvjscholar@fb.com>
2018-06-26 18:38:26 -0700

    Second commit!

--- app/src/main/java/com/example/nvjscholar/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/nvjscholar/simpletodo/MainActivity.java
@@ -2,11 +2,19 @@ package com.example.nvjscholar.simpletodo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
@@ -24,15 +32,38 @@ public class MainActivity extends AppCompatActivity
         //obtain a reference to the ListView created with the layout
         lvItems = (ListView) findViewById(R.id.lvItems);
         //initialize the items list
-        items = new ArrayList<>();
+        readItems();
         //initialize the adapter using the item list
         itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         //wire the adapter to the view
         lvItems.setAdapter(itemsAdapter);
 
         //add some mock items to the list
-        items.add("1st item");
-        items.add("2nd items");
+        //items.add("1st item");
+        //items.add("2nd items");
+
+        setupListViewListener();
+    }
+
+    private void setupListViewListener()
+    {
+        //set the ListView's itemLongClickListener
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener()
+        {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long l)
+            {
+                //remove the item in the list at the index given by position
+                items.remove(position);
+                //notify the adapter that the underlying dataset changed
+                itemsAdapter.notifyDataSetChanged();
+                //store the updated list
+                writeItems();
+                //return true to tell the framework that the long click was consumed
+                Log.i("MainActivity", "Removed item" + position);
+                return true;
+            }
+        });
     }
 
     public void onAddItem(View v)
@@ -43,8 +74,50 @@ public class MainActivity extends AppCompatActivity
         String itemText = etNewItem.getText().toString();
         //add the item to the list via the adapter
         itemsAdapter.add(itemText);
+        //store the updated list
+        writeItems();
         //clear the EditText by setting it to an empty String
         etNewItem.setText(" ");
+        //notification
+        Toast.makeText(getApplicationContext(), "Item added to list", Toast.LENGTH_SHORT).show();
 
+    }
+    //return the file in which the data is stored
+    private File getDataFile()
+    {
+        return new File(getFilesDir(), "todo.txt");
+    }
+
+    //read the items from the file system
+    private void readItems()
+    {
+        try
+        {
+            //create the array using the content in the file
+            items = new ArrayList<String>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        }
+        catch(IOException e)
+        {
+            //print the error to the console
+            e.printStackTrace();
+            //just load an empty list
+            items = new ArrayList<>();
+        }
+
+    }
+
+    //write the items to the filesystem
+    private void writeItems()
+    {
+        try
+        {
+            //save the item list as a line-delimited text file
+            FileUtils.writeLines(getDataFile(), items);
+        }
+        catch(IOException e)
+        {
+            //print the error to the console
+            e.printStackTrace();
+        }
     }
 }
